e860b89740bff35e7d3e3973d58db03e78cab0de
Ssewagudde David <dsewagude10@gmail.com>
2024-09-12 23:48:39 +0300

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Explore from "./pages/Explore";
 import Signup from "./pages/Signup";
 import Offers from "./pages/Offers";
@@ -12,11 +13,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<Signin />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   );
