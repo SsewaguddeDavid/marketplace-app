b5e45529eab33982adab339e6d92b7a0e48535c5
Ssewagudde David <dsewagude10@gmail.com>
2024-09-17 13:28:43 +0300

    add forgot password page

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react-toastify";
+// import arrowRightIcon from "../assets/svg/keyboardArrowRightIcon.svg";
 const ForgotPassword = () => {
-  return <h1>ForgotPassword</h1>;
+  const [email, setEmail] = useState("");
+
+  const onChange = (e) => setEmail(e.target.value);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset email");
+    }
+  };
+  return (
+    <div className="pageContainer">
+      <header>
+        <p className="pageHeader">Forgot password</p>
+      </header>
+      <main>
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            id="email"
+            className="emailInput"
+            placeholder="Email"
+            value={email}
+            onChange={onChange}
+          />
+          <Link className="forgotPasswordLink" to="/signin">
+            Sign In{" "}
+          </Link>
+
+          <div className="signInBar">
+            <div className="signInText">Send Reset Link</div>
+            <button className="signInButton">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 0 24 24"
+                width="24px"
+                fill="currentColor"
+                style={{ color: "#ffffff" }}
+              >
+                <path d="M0 0h24v24H0V0z" fill="none" />
+                <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+              </svg>
+            </button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default ForgotPassword;
